b6825165a900550d5baa01cd19e8084935e07a0c
lfq <lfq@qq.com>
2025-07-19 14:50:21 +0800

    syscall

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
@@ -94,4 +95,20 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+// 收集系统信息
+uint64 sys_sysinfo(void){
+  struct sysinfo info;
+  freebytes(&info.freemem);
+  procnum(&info.nproc);
+
+  uint64 dstaddr;
+  argaddr(0,&dstaddr);
+
+  if(copyout(myproc()->pagetable,dstaddr,(char*)&info,sizeof info)<0){
+    return -1;
+  }
+
+  return 0;
 }
